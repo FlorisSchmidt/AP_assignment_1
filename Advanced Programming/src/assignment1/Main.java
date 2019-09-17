869e9757625c083d252547ca5d74858138c5b785
falksinke <falksinke@gmail.com>
2019-09-17 11:49:00 +0200

    Proxy implementation of java.util.HashSet in "Set" class

--- Advanced Programming/src/assignment1/Main.java
+++ Advanced Programming/src/assignment1/Main.java
@@ -1,10 +1,21 @@
 package assignment1;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		UtilSet set = new UtilSet();
+		set.add('H');
+		set.add('G');
+		System.out.println(Arrays.toString(set.getValues()));
+		set.remove('G');
+		System.out.println(Arrays.toString(set.getValues()));
+		set.add('K');
+		System.out.println(set.isEmpty());
+		set.clear();
+		System.out.println(Arrays.toString(set.getValues()));
+		System.out.println(set.isEmpty());
 	}
 
 }
