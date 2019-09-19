d547d254cee9239075262903c5521bbd1deb8aaf
floris.schmidt <floris.schmidt@hotmail.com>
2019-09-20 00:32:52 +0200

    Setup IdentifierInterface

--- Advanced Programming/src/assignment1/IdentifierInterface.java
+++ Advanced Programming/src/assignment1/IdentifierInterface.java
@@ -18,26 +18,33 @@ public interface IdentifierInterface {
         POST - A new Identifier-object has been made and contains a copy of src.
 
     */
-	
-    void init(char c);
+
+    void init();
     /*
-     * PRE - 
+     * PRE -
      * POST - Identifier has one char
+     */
+
+    boolean add(char c);
+    /*
+     * PRE -
+     * POST - true: the char falls inside the domain and is added
+     * 		  false: the char falls outside of the domain and isn't added
      */
 
     String get();
     /*
-     * PRE - 
+     * PRE -
      * POST - The string that makes up the identifier has been returned
      */
 
     int size();
     /*
-     * PRE - 
+     * PRE -
      * POST - the size of the Identifier is returned
      */
 
-    boolean equals();
+    boolean equals(Identifier comparand);
     /*
      * PRE - the identifiers need to have the same size
      * POST - true: The identifier is the same as the identifier passed
