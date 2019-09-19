d84ff702d97c8129e27c06e770e1225d3f08d65f
Floris Schmidt <Floris@145.108.230.254>
2019-09-19 18:01:50 +0200

    PRE and POST comments added Not final

--- Advanced Programming/src/assignment1/Identifier.java
+++ Advanced Programming/src/assignment1/Identifier.java
@@ -2,7 +2,7 @@ package assignment1;
 
 public interface Identifier {
     /*
-    Elements: characters of type char (Sla op in stringbuffer!)
+    Elements: characters of type char saved in a stingbuffer
     Structure: Linear
     Domain: Only alphanumeric characters, identifier begins with a letter,
             identifier has length of at least 1 character
@@ -13,20 +13,35 @@ public interface Identifier {
         PRE  -
         *   POST -A new Identifier has been made and contains the character x *
         *
-        Identifie
+        Identifier
         PRE  -  *
-         POST - A new NumberStack-object has been made and contains a copy of src.
+        POST - A new Identifier-object has been made and contains a copy of src.
 
     */
+	
     void init(char c);
-    // empties array and places chracter c
+    /*
+     * PRE - 
+     * POST - Identifier has one char
+     */
 
     String get();
+    /*
+     * PRE - 
+     * POST - The string that makes up the identifier has been returned
+     */
 
     int size();
+    /*
+     * PRE - 
+     * POST - the size of the Identifier is returned
+     */
 
     boolean equals();
-
-
+    /*
+     * PRE - the identifiers need to have the same size
+     * POST - true: The identifier is the same as the identifier passed
+     * 		  false: The identifier is not the same as the identifier passed
+     */
 
 }
