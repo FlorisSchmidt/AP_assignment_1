308b2139d4f9e59de7893f79caa1c7f57390f167
floris.schmidt <floris.schmidt@hotmail.com>
2019-09-20 00:35:01 +0200

    Implemented IdentifierInterface

---
+++ Advanced Programming/src/assignment1/Identifier.java
@@ -0,0 +1,59 @@
+package assignment1;
+
+import java.lang.StringBuffer;
+import java.lang.Character;
+
+public class Identifier implements IdentifierInterface {
+
+    private StringBuffer sb;
+    private boolean addWasCalled;
+
+    Identifier(char c){
+        sb = new StringBuffer();
+        addWasCalled = false;
+        init();
+    }
+
+    @Override
+    public void init() {
+        sb.delete(0,sb.length());
+        sb.append('c');
+    }
+
+    @Override
+    public boolean add(char c) {
+        if(testDomain(c) && addWasCalled) {
+            sb.append(c);
+            return true;
+        }
+        if(testDomain(c) && !addWasCalled) {
+            sb.delete(0, 1);
+            sb.append(c);
+            addWasCalled = true;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean testDomain(char c) {
+        if(!addWasCalled && Character.isDigit(c)) {
+            return false;
+        }
+        return Character.isDigit(c) || Character.isLowerCase(c) || Character.isUpperCase(c);
+    }
+
+    @Override
+    public String get() {
+        return sb.toString();
+    }
+
+    @Override
+    public int size() {
+        return sb.length();
+    }
+
+    @Override
+    public boolean equals(Identifier comparand) {
+        return this.get().equals(comparand.get());
+    }
+}
