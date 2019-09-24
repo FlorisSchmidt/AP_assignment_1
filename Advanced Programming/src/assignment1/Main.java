358710e4be8a8a8d059bf4c04b99add71feb3ffb
falksinke <falksinke@gmail.com>
2019-09-24 17:08:12 +0200

    Reading input and adding it to sets now works

--- Advanced Programming/src/assignment1/Main.java
+++ Advanced Programming/src/assignment1/Main.java
@@ -7,15 +7,43 @@ import java.util.regex.Pattern;
 public class Main {
 
 	private PrintStream out;
-
+	public static final String IMPROPER_FORMAT_MESSAGE = "Input not properly formatted";
 	static final int MAX_NUMBER_OF_ELEMENTS = 10;
 
 	private boolean inputContainsCorrectSet(Scanner input, Set set){
-		if(nextChar(input)!='{'){
+		String setString = input.next();
+		if(setString.charAt(0)!='{') {
 			return false;
 		}
-		//TODO split input into identifiers and add them to the set
+		StringBuffer sb = new StringBuffer();
+		int i = 1;
+		int idx = 0;
+		while(setString.charAt(i)!= '}') {
+			while (true) {
+				char c = setString.charAt(i);
+				System.out.println(c);
+				if (c == ',' || c == '}')  {
+					i++;
+					break;
+				}
+				sb.insert(idx, c);
+				idx++;
+				i++;
+			}
+			idx = 0;
+			if (setString.charAt(i-1) == '}') break;
+			addElement(sb.toString(), set);
+			System.out.println("added to set");
+			sb.delete(0, sb.length());
+			idx = 0;
+		}
 		return true;
+	}
+
+	private boolean addElement(String word, Set set) {
+		Identifier i = new Identifier();
+		for (char c: word.toCharArray()) {i.add(c);}
+		return set.add(i);
 	}
 
 	private char nextChar(Scanner in) {
